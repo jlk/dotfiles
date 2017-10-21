2d535be48a80e7a2c1ad93e53fb61f7590c9fbda
John Kinsella <jlk@layeredinsight.com>
2017-10-21 10:47:28 -0700

    Having setup init/update submodules

--- setup.sh
+++ setup.sh
@@ -2,6 +2,10 @@
 
 BACKUP_TIMESTAMP=`date "+%Y%m%d"`
 
+# Pick up submodule(s)
+git submodule init
+git submodule update
+
 # This is fairly quick and dirty - if files we're going to install exist, just move them out of the way as a "backup"
 if [ -d ~/.bashrc ]; then
     mv ~/.bashrc ~/.bashrc-$BACKUP_TIMESTAMP
