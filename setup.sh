609b357ef9e42f253a5211ac8f7357301e2f2b5f
John Kinsella <jlkinsel@gmail.com>
2020-02-28 09:48:52 -0800

    Had checkout path wrong for powerlevel theme

--- setup.sh
+++ setup.sh
@@ -31,7 +31,7 @@ if [ $? -eq 0 ]; then
         mv ~/.oh-my-zsh ~/.oh-my-zah-$BACKUP_TIMESTAMP
     fi
     sh -c zsh/ohmyzsh-install.sh
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/custom/themes/powerlevel10k
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
     cp zsh/zshrc ~/.zshrc
     cp zsh/p10k.zsh ~/.p10k.zsh
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
